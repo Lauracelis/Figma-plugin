4e4af0001278903dc026c089c47d63a7dea5de34
Laura Celis <Lauracelis436@gmail.com>
2023-03-28 19:12:48 -0500

    Feat: Create the project, ui html and code.ts

---
+++ code.js
@@ -0,0 +1,11 @@
+"use strict";
+figma.showUI(__html__, { width: 320, height: 640, title: "Generador de color" });
+figma.ui.onmessage = msg => {
+    if (msg.type === 'actionGenerate') {
+        figma.closePlugin('Han sido generados satisfactoriamente');
+    }
+    else if (msg.type === 'actionExit') {
+        figma.closePlugin();
+    }
+};
+//figma.closePlugin()
